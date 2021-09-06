a999f0e9a7d97840e58d20a3623f972a85161978
pacholoamit <pacholoamit.tech@gmail.com>
2021-09-06 08:04:52 +0800

    Added dark mode theme

--- lib/constants/theme.dart
+++ lib/constants/theme.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 
-final backgroundColor = Color(0xFF0B233C);
+final darkmodeInput = Color(0xFF081A2D);
 
 CustomTheme currentTheme = CustomTheme();
-
-// final theme = ThemeData(primarySwatch: backgroundColor);
 
 class CustomTheme with ChangeNotifier {
   static bool _isDarkTheme = true;
@@ -17,23 +15,24 @@ class CustomTheme with ChangeNotifier {
 
   static ThemeData get lightTheme {
     return ThemeData(
-        primaryColor: Colors.red,
-        backgroundColor: Colors.white,
-        scaffoldBackgroundColor: Colors.white,
-        textTheme: TextTheme(
-          headline1: TextStyle(color: Colors.black),
-          headline2: TextStyle(color: Colors.black),
-          bodyText1: TextStyle(color: Colors.black),
-          bodyText2: TextStyle(color: Colors.black),
-        ));
+      primaryColor: Colors.red,
+      backgroundColor: Colors.white,
+      scaffoldBackgroundColor: Colors.white,
+      textTheme: TextTheme(
+        headline1: TextStyle(color: Colors.black),
+        headline2: TextStyle(color: Colors.black),
+        bodyText1: TextStyle(color: Colors.black),
+        bodyText2: TextStyle(color: Colors.black),
+      ),
+    );
   }
 
   static ThemeData get darkTheme {
     return ThemeData(
-      primaryColor: Colors.black,
-      accentColor: Colors.purple,
-      backgroundColor: Colors.grey,
-      scaffoldBackgroundColor: Colors.grey,
+      primaryColor: Color(0xFF37B7DB),
+      accentColor: Color(0xFF5af5e1),
+      backgroundColor: Color(0xFF0B233C),
+      scaffoldBackgroundColor: Color(0xFF0B233C),
       textTheme: TextTheme(
         headline1: TextStyle(color: Colors.white),
         headline2: TextStyle(color: Colors.white),
